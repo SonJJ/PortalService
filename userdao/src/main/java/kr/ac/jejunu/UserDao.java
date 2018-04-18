1447bd269cae108ce64c05fad487a7e7453c6e2e
SonJJ <zzzzax@naver.com>
2018-04-18 23:34:19 +0900

    미완성, 일단 푸쉬

--- userdao/src/main/java/kr/ac/jejunu/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/UserDao.java
@@ -1,112 +1,34 @@
 package kr.ac.jejunu;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final ConnectionMaker connectionMaker ;
+    private final JdbcContext jdbcContext = new JdbcContext(this);
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    public User get(int id) throws SQLException, ClassNotFoundException{
-    
-        //mysql driver load
-        //sql 작성하고
-        //sql 실행하고
-        //결과를 User 에 매핑하고kk
-        //자원을 해지하고
-        //결과를 리턴한다.
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        User user;
-        try {
-            connection = connectionMaker.getConnection();
-            preparedStatement = connection.prepareStatement("SELECT * FROM userinfo WHERE id = ?");
-            preparedStatement.setInt(1, id);
-            resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-            user = new User();
-            user.setId(resultSet.getInt("id"));
-            user.setName(resultSet.getString("name"));
-            user.setPassword(resultSet.getString("password"));
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return user;
+    public User get(int id) throws SQLException {
+            StatementStrategy statementStrategy = new GetUserStatementStrategy(id);
+        return jdbcContext.jdbcContextForGet(statementStrategy);
     }
 
-    public Integer insert(User user) throws SQLException, ClassNotFoundException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        Integer id;
-        try {
-            connection = connectionMaker.getConnection();
-            preparedStatement = connection.prepareStatement("INSERT INTO userinfo(name, password)" +
-                    "VALUES (?, ?)");
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.executeUpdate();
-
-            preparedStatement = connection.prepareStatement(
-                    "SELECT last_insert_id()");
-            resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-
-            id = resultSet.getInt(1);
-        } finally {
-            if (resultSet != null) {
-                try {
-                    resultSet.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (preparedStatement != null) {
-                try {
-                    preparedStatement.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }
-
-        return id;
+    public Integer insert(User user) throws SQLException{
+            StatementStrategy statementStrategy = new InsertUserStatementStrategy(user);
+        return jdbcContext.jdbcContextForInsert(statementStrategy);
     }
 
-//    abstract public Connection getConnection() throws ClassNotFoundException, SQLException;
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        return connectionMaker.getConnection();
+    public void update(User user) throws SQLException{
+        StatementStrategy statementStrategy = new UpdateUserstStatementStrategy(user);
+
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
+    }
+
+    public void delete(Integer id) throws SQLException {
+        StatementStrategy statementStrategy = new DeleteUserStatementStrategy(id);
+
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 }
